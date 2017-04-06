740c6d349e318674e8f7cf8125077733944bc96f
greenfavo <greenfavo@qq.com>
2017-04-06 16:19:28 +0800

    代码格式化

--- src/views/Shelf.vue
+++ src/views/Shelf.vue
@@ -5,19 +5,20 @@
        <mu-tab value="borrow" title="借阅图书"/>
        <mu-tab value="lend" title="借出图书" />
    </mu-tabs>
-   <div class="main mT20">
-     <div v-if="activeTab==='private'">
-       <book-grid ></book-grid>
-       <mu-float-button icon="add" secondary class="addBookBtn" ref="button" @click="toggle"/>
-       <mu-popover :trigger="trigger" :open="open" @close="handleClose" :anchorOrigin="{vertical:'top',horizontal:'left'}">
+    <div class="main mT20">
+      <div v-if="activeTab==='private'">
+        <book-grid ></book-grid>
+        <mu-float-button icon="add" secondary class="addBookBtn" ref="button" @click="toggle"/>
+        <mu-popover :trigger="trigger" :open="open" @close="handleClose" :anchorOrigin="{vertical:'top',horizontal:'left'}">
          <mu-menu>
            <mu-menu-item title="手动添加" href="#/addBook"/>
            <mu-menu-item title="扫码添加" />
          </mu-menu>
-       </mu-popover>
-     </div>
+        </mu-popover>
+      </div>
      <book-grid v-if="activeTab==='borrow'"></book-grid>
      <book-grid v-if="activeTab==='lend'"></book-grid>
+    </div>
   </div>
 </template>
 
