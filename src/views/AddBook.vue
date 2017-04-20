7a04a2b7cc201a0c5b4ede65c8c6b3286417f2ee
greenfavo <greenfavo@qq.com>
2017-04-20 15:45:56 +0800

    调试

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -51,27 +51,41 @@ export default {
       return this.$route.query.from || ''
     },
     bookInfo () {
-      console.log('from= ', this.from)
       if (this.from === 'scan') {
         console.log('bookInfo', this.$store.state.book)
         return this.$store.state.book
       }
     }
   },
+  watch: {
+    'bookInfo' (val) {
+      if (val) {
+        console.log('图书信息', this.bookInfo)
+        console.log('addBook', typeof this.bookInfo)
+        this.ISBN = this.$route.query.isbn
+        this.name = this.bookInfo.title
+        this.author = this.bookInfo.author[0]
+        this.publish = this.bookInfo.publisher
+        this.publishDate = this.bookInfo.pubdate
+        this.summary = this.bookInfo.summary
+        this.cover = this.bookInfo.image
+      }
+    }
+  },
   mounted () {
     this.$store.commit('HIDDEN_NAVBAR')
     this.$store.commit('SET_BACK_TITLE', '添加图书')
-    if (this.from === 'scan') {
-      console.log('图书信息', this.bookInfo)
-      console.log('addBook', typeof this.bookInfo)
-      this.ISBN = this.$route.query.isbn
-      this.name = this.bookInfo.title
-      this.author = this.bookInfo.author[0]
-      this.publish = this.bookInfo.publisher
-      this.publishDate = this.bookInfo.pubdate
-      this.summary = this.bookInfo.summary
-      this.cover = this.bookInfo.image
-    }
+    // if (this.from === 'scan') {
+    //   console.log('图书信息', this.bookInfo)
+    //   console.log('addBook', typeof this.bookInfo)
+    //   this.ISBN = this.$route.query.isbn
+    //   this.name = this.bookInfo.title
+    //   this.author = this.bookInfo.author[0]
+    //   this.publish = this.bookInfo.publisher
+    //   this.publishDate = this.bookInfo.pubdate
+    //   this.summary = this.bookInfo.summary
+    //   this.cover = this.bookInfo.image
+    // }
   },
   components: {
     Upload
