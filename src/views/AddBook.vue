148efb06e22baa4e9c1cf7631b6fbb4e1406fdab
greenfavo <greenfavo@qq.com>
2017-04-20 13:13:50 +0800

    调试

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -51,7 +51,8 @@ export default {
     this.$store.commit('HIDDEN_NAVBAR')
     this.$store.commit('SET_BACK_TITLE', '添加图书')
     if (this.from === 'scan') {
-      console.log(`图书信息:${this.bookInfo}`)
+      console.log('图书信息', this.bookInfo)
+      console.log(typeof this.bookInfo)
       this.ISBN = this.$route.query.isbn
       this.name = this.bookInfo.title
       this.author = this.bookInfo.author
